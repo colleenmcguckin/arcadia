7d4921ec81a5491d563564f1d38fc88ca9f89ef8
colleenmcguckin <colleenmcguckin@gmail.com>
2017-07-16 14:25:16 -0700

    seed pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+Page.create!(
+  title: 'welcome',
+  content: 'welcome page'
+)
+
+Page.create!(
+  title: 'home',
+  content: 'home page'
+)
+
+Page.create!(
+  title: 'mission',
+  content: 'mission page'
+)
+
+Page.create!(
+  title: 'rentals',
+  content: 'rentals page'
+)
+
+Page.create!(
+  title: 'visit us',
+  content: 'visit us page'
+)
