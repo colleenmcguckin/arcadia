98cfa4cdc119b0a798170e71e2123d8e9fa808e7
colleenmcguckin <colleenmcguckin@gmail.com>
2017-07-16 13:31:07 -0700

    setup activeadmin

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'friendly_id', '~> 5.1.0'
 
 gem 'hamlit'
 gem 'devise'
+gem 'activeadmin'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#welcome'
   resources :pages, only: [:show, :welcome], path: ''
