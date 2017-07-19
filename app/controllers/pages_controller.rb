e2f9551964a2404315e7b6399495a703907b3d93
colleenmcguckin <colleenmcguckin@gmail.com>
2017-07-18 23:20:00 -0700

    begin styling home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,9 +6,8 @@ class PagesController < ApplicationController
   def show
     @page = Page.friendly.find(params[:id])
     @team_members = TeamMember.all if %w[team].include? @page.title
-    @events = Event.all if %w[events].include? @page.title
+    @events = Event.upcoming.first(3) if %w[events home].include? @page.title
     @news_updates = NewsUpdate.shown if %w[news].include? @page.title
     @giving_options = GivingOption.all if %w[give].include? @page.title
-
   end
 end
